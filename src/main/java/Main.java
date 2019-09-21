3e038de84425aceb7e93ac14929eca67f666dafe
AlekseiILitvinov <litvinovai988@gmail.com>
2019-09-21 19:03:58 +0300

    Initial commit. Realized Post class.

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		Post post = new Post();
+		//initialized with 0 values;
+		System.out.println(post.id);
+
+		post.id = 1;
+		System.out.println(post.id);
+
+		String text = "IT Academy";
+		System.out.println(text);
+	}
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,13 @@
+public class Post {
+	//props and fields here
+	long id;
+	long created;
+	String author;
+	String avatarUrl;
+	String content;
+	int likes;
+	int comments;
+	int views;
+	char ch;
+	boolean removed;
+}
